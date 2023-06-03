f55c10f56175310afdb90e0dcc5753e8601a8182
AdamJSwain <adamjswain@gmail.com>
2023-06-03 21:52:45 +0200

    Updated with Login which leads to NavigationDrawer (home page)

--- app/src/main/java/com/example/opsc7311_prototypeapp/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val intent = Intent(this, login::class.java)
+        startActivity(intent)
     }
 
     fun redirectToNavigationDrawer(view: View) {
--- app/src/main/java/com/example/opsc7311_prototypeapp/login.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/login.kt
@@ -1,11 +1,40 @@
 package com.example.opsc7311_prototypeapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val tvUsername = findViewById<TextView>(R.id.etUsername)
+
+        val tvPassword = findViewById<TextView>(R.id.etPassword)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener()
+        {
+
+            if (tvUsername.text.toString().equals("Admin") && tvPassword.text.toString().equals("Password"))
+            {
+                Toast.makeText(this@login, "You have successfully logged in, Welcome Admin", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, NavigationDrawer::class.java)
+                startActivity(intent)
+            }
+            else
+            {
+                Toast.makeText(this@login, "Please provide a registered username and password", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
