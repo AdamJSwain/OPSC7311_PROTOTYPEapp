6515e4649a1bbb8f32349f41dd21fcc9b2aabcef
Saurav Sewparsad <sewparsad60@gmail.com>
2023-06-05 12:23:33 +0000

    Categories list update

--- app/src/main/java/com/example/opsc7311_prototypeapp/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/ui/categories/CategoriesFragment.kt
@@ -4,54 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.opsc7311_prototypeapp.R
 import com.example.opsc7311_prototypeapp.databinding.FragmentCategoriesBinding
+
 
 class CategoriesFragment : Fragment() {
 
     private var _binding: FragmentCategoriesBinding? = null
-
+    private val binding get() = _binding!!
     // This property is only valid between onCreateView and
     // onDestroyView.
-    // TestComment
-    //testComment2
-    private val binding get() = _binding!!
-    private lateinit var spinnerCategoryColor: Spinner
-    private lateinit var buttonCreateCategory: Button
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val rootView = inflater.inflate(R.layout.fragment_categories, container, false)
+    private lateinit var ListCategory: ListView
+    private lateinit var itemList: ArrayList<String>
+    private lateinit var adapter: ArrayAdapter<String>
 
-        spinnerCategoryColor = rootView.findViewById(R.id.spinnerCategoryColor)
-        buttonCreateCategory = rootView.findViewById(R.id.buttonCreateCategory)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_categories, container, false)
+        ListCategory = view.findViewById(R.id.ListCategory)
+        itemList = ArrayList()
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, itemList)
+        ListCategory.adapter = adapter
 
-        // Get the array of colors from resources
-        val colorsArray = resources.getStringArray(R.array.category_colors)
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, colorsArray)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerCategoryColor.adapter = adapter
-
-        // Add a listener to the button
+        val buttonCreateCategory: Button = view.findViewById(R.id.buttonCreateCategory)
         buttonCreateCategory.setOnClickListener {
-            val selectedColor = spinnerCategoryColor.selectedItem.toString()
-            //val categoryName = editTextCategoryName.text.toString()
-            // Perform the action with the selected color and category name
+            val newItem = "New Item"
+            addItemToList(newItem)
         }
 
-        return rootView
+        val ButtonUpdate: Button = view.findViewById(R.id.ButtonUpdate)
+        ButtonUpdate.setOnClickListener {
+            val index = 0  // Specify the index of the item to update
+            val updatedItem = "Updated Item"
+            updateItemInList(index, updatedItem)
+        }
+
+
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    // Add items to the ListView
+    private fun addItemToList(item: String) {
+        itemList.add(item)
+        adapter.notifyDataSetChanged()
     }
+
+    // Update items in the ListView
+    private fun updateItemInList(index: Int, updatedItem: String) {
+        itemList[index] = updatedItem
+        adapter.notifyDataSetChanged()
+    }
+
+
+
 }
