d0b5dbf81c2790ed203af9fff1350edce7ca78f6
RobboVC <robynma03@gmail.com>
2023-06-05 17:12:09 +0200

    Update and added categories tab to project and made minor layout/design changes

---
+++ app/src/main/java/com/example/opsc7311_prototypeapp/ui/categories_view/CategoriesViewFragment.kt
@@ -0,0 +1,36 @@
+package com.example.opsc7311_prototypeapp.ui.categories_view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.opsc7311_prototypeapp.databinding.FragmentCategoriesViewBinding
+
+class CategoriesViewFragment : Fragment() {
+
+    private var _binding: FragmentCategoriesViewBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        _binding = FragmentCategoriesViewBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
