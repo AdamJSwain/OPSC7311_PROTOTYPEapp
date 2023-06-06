6dc483a2235fd56db8fa14ff180d17247f84e983
Saurav Sewparsad <sewparsad60@gmail.com>
2023-06-06 13:19:59 +0000

    Entries page adding variables

--- app/src/main/java/com/example/opsc7311_prototypeapp/ui/entries/EntriesFragment.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/ui/entries/EntriesFragment.kt
@@ -1,16 +1,27 @@
 package com.example.opsc7311_prototypeapp.ui.entries
 
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.opsc7311_prototypeapp.R
 import com.example.opsc7311_prototypeapp.Worker
 import com.example.opsc7311_prototypeapp.databinding.FragmentEntriesBinding
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.opsc7311_prototypeapp.TimeSheetEntry
+import java.util.*
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
+import android.widget.*
+import java.io.IOException
 
 class EntriesFragment : Fragment() {
 
@@ -21,21 +32,71 @@ class EntriesFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var spinnerCategory: Spinner
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var datePicker: DatePicker
+    private lateinit var timePickerStartTime: TimePicker
+    private lateinit var timePickerEndTime: TimePicker
+    private lateinit var editTextDescription: EditText
+    private lateinit var buttonAddPhoto: Button
+    private lateinit var buttonSaveEntry: Button
+
+    private var selectedDate: Date? = null
+    private var selectedStartTime: Date? = null
+    private var selectedEndTime: Date? = null
+    private var selectedImage: Bitmap? = null
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_entries, container, false)
 
-        spinnerCategory= view.findViewById(R.id.spinnerCategory)
+        datePicker = view.findViewById(R.id.datePicker)
+        timePickerStartTime = view.findViewById(R.id.timePickerStartTime)
+        timePickerEndTime = view.findViewById(R.id.timePickerEndTime)
+        spinnerCategory = view.findViewById(R.id.spinnerCategory)
+        editTextDescription = view.findViewById(R.id.editTextDescription)
+        buttonAddPhoto = view.findViewById(R.id.buttonAddPhoto)
+        buttonSaveEntry = view.findViewById(R.id.buttonSaveEntry)
 
-        val bundle = arguments
-        if (bundle != null && bundle.containsKey("enteredText")) {
-            val enteredText = bundle.getString("enteredText")
-
-            // Create an ArrayAdapter with a single item (entered text)
-            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listOf(enteredText))
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerCategory.adapter = adapter
-        }
-
+        buttonAddPhoto.setOnClickListener { dispatchTakePictureIntent() }
+        buttonSaveEntry.setOnClickListener { submitTimesheetEntry() }
         return view
     }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap?
+            selectedImage = imageBitmap
+        }
+    }
+
+    private fun submitTimesheetEntry() {
+        val description = editTextDescription.text.toString()
+
+        val selectedCalendar = Calendar.getInstance()
+        selectedCalendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        selectedDate = selectedCalendar.time
+
+        selectedCalendar.set(Calendar.HOUR_OF_DAY, timePickerStartTime.hour)
+        selectedCalendar.set(Calendar.MINUTE, timePickerStartTime.minute)
+        selectedStartTime = selectedCalendar.time
+
+        selectedCalendar.set(Calendar.HOUR_OF_DAY, timePickerEndTime.hour)
+        selectedCalendar.set(Calendar.MINUTE, timePickerEndTime.minute)
+        selectedEndTime = selectedCalendar.time
+    }
 }
+
