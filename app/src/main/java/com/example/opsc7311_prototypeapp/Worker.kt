367460ce0c92813df3cd6781d800a3ffa575b7e3
AdamJSwain <adamjswain@gmail.com>
2023-06-27 21:15:55 +0200

    Login and Register now use firebase and use an email and password for signing in. Hopefully the rest of you will be able to use my firebase for this, otherwise problems. let me know if it doesnt work

--- app/src/main/java/com/example/opsc7311_prototypeapp/Worker.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/Worker.kt
@@ -9,6 +9,32 @@ companion object{
    val catList = mutableListOf<Category>()
 
    val updatedObjects = mutableListOf<Category>()
+
+   var userInfo = ""
+
+   fun isValidPassword(password: String): Boolean {
+
+      if(password.length < 8){
+         return false
+      }
+      var hasLower = false
+      var hasUpper = false
+      var hasNumber = false
+      for (cha in password){
+         if (Character.isLowerCase(cha)){
+            hasLower = true
+         }
+         else if (Character.isUpperCase(cha)){
+            hasUpper = true
+         }
+         else if (Character.isDigit(cha)){
+            hasNumber = true
+         }
+
+      }
+
+      return hasLower && hasUpper && hasNumber
+   }
    fun SortObjects(){
       for (Category in catList) {
          val existingObj = updatedObjects.find { it.categoryName == Category.categoryName }
