da6c520d38a9b5babf33cfbc1e053435b21e5a00
AdamJSwain <adamjswain@gmail.com>
2023-07-02 18:20:07 +0200

    Added collapsable date pickers to both view entries and view categories, still need to add the hours spent together. getting multiple items from 1 query is being hard

--- app/src/main/java/com/example/opsc7311_prototypeapp/ui/categories_view/CategoriesViewFragment.kt
+++ app/src/main/java/com/example/opsc7311_prototypeapp/ui/categories_view/CategoriesViewFragment.kt
@@ -1,6 +1,7 @@
 package com.example.opsc7311_prototypeapp.ui.categories_view
 
 //all imports required
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import androidx.fragment.app.Fragment
 import com.example.opsc7311_prototypeapp.Category
 import com.example.opsc7311_prototypeapp.Worker
 import com.example.opsc7311_prototypeapp.databinding.FragmentCategoriesViewBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 //this fragment is for viewing all the categories the user has entered
 class CategoriesViewFragment : Fragment() {
@@ -26,9 +29,51 @@ class CategoriesViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCategoriesViewBinding.inflate(inflater, container, false)
-
+        val startText = binding.editTextStartDate
+        val endText = binding.editTextEndDate
         val adapter: ArrayAdapter<*>
+        startText.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+        endText.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
 
+        var cal = Calendar.getInstance()
+
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val myFormat = "yyyy.MM.dd" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+                startText.text = sdf.format(cal.time)
+
+            }
+        val dateSetListener1 =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val myFormat = "yyyy.MM.dd" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+
+                endText.text = sdf.format((cal.time))
+            }
+
+        startText.setOnClickListener {
+            DatePickerDialog(
+                requireContext(), dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+        endText.setOnClickListener {
+            DatePickerDialog(
+                requireContext(), dateSetListener1,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
         binding.buttonShowCategories.setOnClickListener()
         {
             Worker.SortObjects()
